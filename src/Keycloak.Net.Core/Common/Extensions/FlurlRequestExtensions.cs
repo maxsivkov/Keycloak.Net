9b6fcca06acd8e10778c09977dfe6ae5b1cf4ddd
Max Sivkov <maxsivkov@gmail.com>
2023-04-19 14:45:30 +0300

    Implement refresh token

--- src/Keycloak.Net.Core/Common/Extensions/FlurlRequestExtensions.cs
+++ src/Keycloak.Net.Core/Common/Extensions/FlurlRequestExtensions.cs
@@ -8,71 +8,8 @@ namespace Keycloak.Net.Common.Extensions
 {
     public static class FlurlRequestExtensions
     {
-        private static async Task<string> GetAccessTokenAsync(string url, string realm, string userName, string password, KeycloakOptions options = null)
-        {
-            options ??= new KeycloakOptions();
-            var result = await url
-                .AppendPathSegment($"{options.Prefix}/realms/{realm}/protocol/openid-connect/token")
-                .WithHeader("Accept", "application/json")
-                .PostUrlEncodedAsync(new List<KeyValuePair<string, string>>
-                {
-                    new KeyValuePair<string, string>("grant_type", "password"),
-                    new KeyValuePair<string, string>("username", userName),
-                    new KeyValuePair<string, string>("password", password),
-                    new KeyValuePair<string, string>("client_id", options.AdminClientId)
-                })
-                .ReceiveJson().ConfigureAwait(false);
-
-            string accessToken = result
-                .access_token.ToString();
-
-            return accessToken;
-        }
-
-        private static string GetAccessToken(string url, string realm, string userName, string password, KeycloakOptions options = null) => GetAccessTokenAsync(url, realm, userName, password, options).GetAwaiter().GetResult();
-
-        private static async Task<string> GetAccessTokenAsync(string url, string realm, string clientSecret, KeycloakOptions options = null)
-        {
-            options ??= new KeycloakOptions();
-            var result = await url
-                .AppendPathSegment($"{options.Prefix}/realms/{realm}/protocol/openid-connect/token")
-                .WithHeader("Content-Type", "application/x-www-form-urlencoded")
-                .PostUrlEncodedAsync(new List<KeyValuePair<string, string>>
-                {
-                    new KeyValuePair<string, string>("grant_type", "client_credentials"),
-                    new KeyValuePair<string, string>("client_secret", clientSecret),
-                    new KeyValuePair<string, string>("client_id", options.AdminClientId)
-                })
-                .ReceiveJson().ConfigureAwait(false);
-
-            string accessToken = result
-                .access_token.ToString();
-
-            return accessToken;
-        }
-
-        private static string GetAccessToken(string url, string realm, string clientSecret, KeycloakOptions options = null) => GetAccessTokenAsync(url, realm, clientSecret, options).GetAwaiter().GetResult();
-
-        public static IFlurlRequest WithAuthentication(this IFlurlRequest request, Func<string> getToken, string url, string realm, string userName, string password, string clientSecret, KeycloakOptions options = null)
-        {
-            string token = null;
-
-            if (getToken != null)
-            {
-                token = getToken();
-            }
-            else if (clientSecret != null)
-            {
-                token = GetAccessToken(url, realm, clientSecret, options);
-            }
-            else
-            {
-                token = GetAccessToken(url, realm, userName, password, options);
-            }
-
-            return request.WithOAuthBearerToken(token);
-        }
-
+        public static IFlurlRequest WithAuthentication(this IFlurlRequest request, string authenticationRealm, KeycloakClient cli)
+             => request.WithOAuthBearerToken(cli.GetAccessToken(authenticationRealm));
         public static IFlurlRequest WithForwardedHttpHeaders(this IFlurlRequest request, ForwardedHttpHeaders forwardedHeaders)
         {
             if (!string.IsNullOrEmpty(forwardedHeaders?.forwardedFor))
--- src/Keycloak.Net.Core/KeycloakClient.cs
+++ src/Keycloak.Net.Core/KeycloakClient.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
 using Flurl.Http.Configuration;
 using Keycloak.Net.Common.Extensions;
+using Keycloak.Net.Models.Clients;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -17,17 +22,28 @@ namespace Keycloak.Net
             
         });
 
-        private readonly Url _url;
-        private readonly string _userName;
-        private readonly string _password;
-        private readonly string _clientSecret;
-        private readonly Func<string> _getToken;
-        private readonly KeycloakOptions _options;
+        protected internal readonly Url _url;
+        protected internal readonly string _userName;
+        protected internal readonly string _password;
+        protected internal readonly string _clientSecret;
+        protected internal readonly Func<string> _getToken;
+        protected internal readonly KeycloakOptions _options;
+
+
+
+        private readonly SemaphoreSlim _semaphore;
+        private string? _access_token, _refresh_token;
+        private int? _expires_in;
+        private DateTime? _token_expiration;
 
         private KeycloakClient(string url, KeycloakOptions options)
         {
             _url = url;
             _options = options ?? new KeycloakOptions();
+            _access_token = _refresh_token = null;
+            _token_expiration = DateTime.Now;
+            _expires_in = 60;
+            _semaphore = new SemaphoreSlim(1, 1);
         }
 
         public KeycloakClient(string url, string userName, string password, KeycloakOptions options = null) 
@@ -54,12 +70,65 @@ namespace Keycloak.Net
             _serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
         }
 
+        private dynamic TokenEndpoint(string url, string realm, IEnumerable<KeyValuePair<string, string>> data) => TokenEndpointAsync(url, realm, data).GetAwaiter().GetResult();
+
+        private async Task<dynamic> TokenEndpointAsync(string url, string realm, IEnumerable<KeyValuePair<string, string>> data) =>
+            await url
+            .AppendPathSegment($"{_options.Prefix}/realms/{realm}/protocol/openid-connect/token")
+            .WithHeader("Content-Type", "application/x-www-form-urlencoded")
+            .WithHeader("Accept", "application/json")
+            .PostUrlEncodedAsync(data)
+            .ReceiveJson().ConfigureAwait(false);
+
+        protected IEnumerable<KeyValuePair<string, string>> TokenEndpointData() =>
+            _clientSecret != null ? new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials"),
+                new KeyValuePair<string, string>("client_secret", _clientSecret),
+                new KeyValuePair<string, string>("client_id", _options.AdminClientId)
+            } : new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", _userName),
+                new KeyValuePair<string, string>("password", _password),
+                new KeyValuePair<string, string>("client_id", _options.AdminClientId)
+            };
+        protected async Task<string> RetrieveAccessTokenAsync(string authenticationRealm)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                if (_expires_in is not null && DateTime.Now.AddSeconds(Convert.ToDouble(_expires_in) / 3) > _token_expiration)
+                {
+                    var data = _refresh_token is not null ? new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("grant_type", "refresh_token"),
+                        new KeyValuePair<string, string>("client_id", _options?.AdminClientId),
+                        new KeyValuePair<string, string>("refresh_token", _refresh_token)
+                    } : TokenEndpointData();
+                    //Trace.WriteLine($"{(_refresh_token is null ? "NEW" : "REFRESHED")} TOKEN RECEIVED");
+                    var result = TokenEndpoint(_url, _options.AuthenticationRealmName ?? authenticationRealm, data);
+                    _access_token = result.access_token.ToString();
+                    _refresh_token = result.refresh_token.ToString();
+                    _expires_in = Convert.ToInt32(result.expires_in.ToString());
+                    _token_expiration = DateTime.Now.AddSeconds(Convert.ToDouble(_expires_in));
+                }
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+            return _access_token;
+        }
+
+        protected string RetrieveAccessToken(string authenticationRealm) => RetrieveAccessTokenAsync(authenticationRealm).GetAwaiter().GetResult();
+
+        public string GetAccessToken(string authenticationRealm) =>
+            _getToken != null ? _getToken() : RetrieveAccessToken(authenticationRealm);
         private IFlurlRequest GetBaseUrl(string authenticationRealm) => new Url(_url)
             .AppendPathSegment(_options.Prefix)
             .ConfigureRequest(settings => settings.JsonSerializer = _serializer)
-            .WithAuthentication(_getToken, _url,
-                _options.AuthenticationRealmName ?? authenticationRealm
-                , _userName, _password, _clientSecret, _options);
+            .WithAuthentication(authenticationRealm, this);
     }
 
     public class KeycloakOptions
